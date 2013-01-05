129fc2b922f4fbf5eb4a1ef151c4c21060029354
Dan Stocker <dan@kwaia.com>
2013-01-05 12:01:08 +0100

    dessert & troop upped to 0.2.2

--- js/sntls.js
+++ js/sntls.js
@@ -9,6 +9,6 @@ var sntls = {},
 
 // adding Node.js dependencies
 if (typeof exports === 'object' && typeof require === 'function') {
-    dessert = require('dessert-0.2.1').dessert;
-    troop = require('troop-0.2.1').troop;
+    dessert = require('dessert-0.2.2').dessert;
+    troop = require('troop-0.2.2').troop;
 }
