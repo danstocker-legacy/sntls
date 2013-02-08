6b86e6d2a5cc7897aa3d96f0788512c74d051090
Dan Stocker <dan@kwaia.com>
2013-02-08 15:31:31 +0100

    Stats assertions

--- js/Stats.js
+++ js/Stats.js
@@ -6,4 +6,19 @@
 /*global dessert, troop */
 troop.promise('sntls.Stats', function (sntls) {
     sntls.Stats = sntls.Collection.of(sntls.Stat);
+
+    dessert.addTypes({
+        isStats: function (expr) {
+            return this.isClass(expr) &&
+                   (expr.isA(sntls.Stat) ||
+                    expr.isA(sntls.Stats));
+        },
+
+        isStatsOptional: function (expr) {
+            return typeof expr === 'undefined' ||
+                   this.isClass(expr) &&
+                   (expr.isA(sntls.Stat) ||
+                    expr.isA(sntls.Stats));
+        }
+    });
 });
