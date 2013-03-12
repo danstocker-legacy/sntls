36d69f664b73f7810f8a7ffd151c6ed1f784cede
Dan Stocker <dan@kwaia.com>
2013-03-12 10:51:19 +0100

    stateful trait

---
+++ js/Stateful.js
@@ -0,0 +1,80 @@
+/**
+ * Stateful Trait
+ *
+ * Classes that have this trait may define orthogonal state layers.
+ */
+/* global dessert, troop, sntls */
+troop.promise(sntls, 'Stateful', function () {
+    /**
+     * @class sntls.Stateful
+     * @extends troop.Base
+     */
+    sntls.Stateful = troop.Base.extend()
+        .addMethod(/** @lends sntls.Stateful */{
+            /**
+             * Adds properties required by trait.
+             */
+            initStateful: function () {
+                this.addPublic(/** @lends sntls.Stateful */{
+                    /**
+                     * (Unspecified) collection of strings
+                     * Stores current state names for each state layer
+                     * @type {sntls.Collection}
+                     */
+                    currentStates: sntls.Collection.create()
+                });
+            },
+
+            /**
+             * Adds a state layer.
+             * A state layer is a set of mutually exclusive states,
+             * eg. enabled-disabled, or open-closed.
+             * @param {string} layerName
+             * @param {sntls.StateMatrix} stateMatrix
+             * @static
+             */
+            addStateLayer: function (layerName, stateMatrix) {
+                dessert
+                    .isString(layerName)
+                    .isStateMatrix(stateMatrix);
+
+                if (!this.hasOwnProperty('stateMatrices')) {
+                    this.addConstant(/** @lends sntls.Stateful */{
+                        /**
+                         * @type {sntls.StateMatrixCollection}
+                         */
+                        stateMatrices: sntls.StateMatrixCollection.create()
+                    });
+                }
+
+                // setting state matrix as layer
+                this.stateMatrices.setItem(layerName, stateMatrix);
+
+                return this;
+            },
+
+            /**
+             * Changes state on specified state layer.
+             * @param {string} stateName Destination state
+             * @param {string} layerName Layer name
+             */
+            changeStateTo: function (stateName, layerName) {
+                dessert.isString(layerName);
+
+                var currentStateName = this.currentStates.getItem(layerName),
+                    methodName;
+
+                if (currentStateName) {
+                    methodName = this.stateMatrices.getItem(layerName)
+                        .getLoad(currentStateName, stateName);
+                }
+
+                // calling handler and changing state
+                if (!methodName || this[methodName]() !== false) {
+                    this.currentStates.setItem(layerName, stateName);
+                }
+
+                return this;
+            }
+        });
+});
---
+++ js/Stateful.test.js
@@ -0,0 +1,83 @@
+/* global sntls, troop, module, test, ok, equal, strictEqual, deepEqual, notDeepEqual, raises, expect */
+(function (Stateful) {
+    module("Stateful");
+
+    var stateMatrix = sntls.StateMatrix.create()
+        .addEdge('open', 'closed', 'close')
+        .addEdge('closed', 'open', 'open');
+
+    test("Layer addition", function () {
+        /**
+         * @class Widget
+         * @extends sntls.Stateful
+         */
+        var Widget = troop.Base.extend()
+                .addTrait(Stateful),
+            result;
+
+        raises(function () {
+            Widget.addStateLayer();
+        }, "Invalid arguments");
+
+        raises(function () {
+            Widget.addStateLayer('layer', 'foo');
+        }, "Invalid state matrix argument");
+
+        result = Widget.addStateLayer('openable', stateMatrix);
+
+        strictEqual(result, Widget, "Layer addition returns caller");
+
+        deepEqual(Widget.stateMatrices.items, {
+            'openable': stateMatrix
+        });
+    });
+
+    test("State transition", function () {
+        /**
+         * @class Widget
+         * @extends sntls.Stateful
+         */
+        var Widget = troop.Base.extend()
+                .addTrait(Stateful)
+                .addStateLayer('layer', stateMatrix)
+                .addMethod({
+                    init: function () {
+                        this.initStateful();
+                    },
+
+                    open: function () {
+                        isOpen = true;
+                    },
+
+                    close: function () {
+                        isClosed = true;
+                    }
+                }),
+            widget = Widget.create(),
+            isOpen, isClosed,
+            result;
+
+        ok(widget.currentStates.isA(sntls.Collection), "Current state container collection");
+        deepEqual(widget.currentStates.items, {}, "Current states container empty");
+
+        result = widget.changeStateTo('open', 'layer');
+
+        strictEqual(result, widget, "State change returns self");
+        deepEqual(widget.currentStates.items, {
+            layer: 'open'
+        }, "Available layers");
+
+        widget.changeStateTo('closed', 'layer');
+
+        deepEqual(widget.currentStates.items, {
+            layer: 'closed'
+        }, "Available layers");
+
+        ok(isClosed, "Open -> closed state transition done");
+        ok(!isOpen, "Closed -> open state transition NOT done");
+
+        widget.changeStateTo('open', 'layer');
+
+        ok(isOpen, "Closed -> open state transition done");
+    });
+}(sntls.Stateful));
