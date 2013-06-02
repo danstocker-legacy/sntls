0729bf257be85754d2e43208b63c52e8f31448c6
Dan Stocker <dan@kwaia.com>
2013-06-02 16:30:02 +0200

    grunt based build & testing

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+/*jshint node:true */
+module.exports = function (grunt) {
+    "use strict";
+
+    var params = {
+        files: [
+            'js/namespace.js',
+            'js/utils.js',
+            'js/data/Hash.js',
+            'js/data/Dictionary.js',
+            'js/data/StringDictionary.js',
+            'js/data/Collection.js',
+            'js/data/JournalingCollection.js',
+            'js/data/OrderedList.js',
+            'js/data/OrderedStringList.js',
+            'js/data/Path.js',
+            'js/data/Tree.js',
+            'js/data/collections/ArrayCollection.js',
+            'js/data/collections/DateCollection.js',
+            'js/data/collections/StringCollection.js',
+            'js/behavior/Profile.js',
+            'js/behavior/Profiled.js',
+            'js/behavior/StateMatrix.js',
+            'js/behavior/Stateful.js',
+            'js/exports.js'
+        ],
+
+        test: [
+            'js/behavior/jsTestDriver.conf',
+            'js/data/jsTestDriver.conf',
+            'js/jsTestDriver.conf'
+        ],
+
+        globals: {
+            dessert: true,
+            troop  : true
+        }
+    };
+
+    // invoking common grunt process
+    require('common-gruntfile')(grunt, params);
+};
