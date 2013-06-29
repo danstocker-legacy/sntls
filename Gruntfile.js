366de0a02dbb79b88bedf35377ca9ad8dfd9ac3f
Dan Stocker <dan@kwaia.com>
2013-06-29 16:50:54 +0200

    QueryPattern added to output file list

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,6 +14,7 @@ module.exports = function (grunt) {
             'js/data/OrderedList.js',
             'js/data/OrderedStringList.js',
             'js/tree/Path.js',
+            'js/tree/QueryPattern.js',
             'js/tree/Query.js',
             'js/tree/TreeWalker.js',
             'js/tree/IterativeTreeWalker.js',
