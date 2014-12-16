0fd4f251c6e33759a538a76c9094a722d0da7563
Dan Stocker <dan.stocker@mangahigh.com>
2014-12-16 15:18:13 +0000

    revisits Tree.unsetPath() unit tests

--- js/tree/Tree.test.js
+++ js/tree/Tree.test.js
@@ -241,17 +241,16 @@
         expect(12);
 
         var tree = sntls.Tree.create({
-                a: {d: {}, e: {}, f: {
-                    g: {}, h: {
-                        i: {j: {k: {l: {
-                            n: {o: "p"}
-                        }, m         : {}}}}
-                    }
-                }},
-                b: {},
-                c: {}
-            }),
-            result;
+            a: {d: {}, e: {}, f: {
+                g: {}, h: {
+                    i: {j: {k: {l: {
+                        n: {o: "p"}
+                    }, m         : {}}}}
+                }
+            }},
+            b: {},
+            c: {}
+        });
 
         tree.unsetPath('foo>bar>baz'.toPath());
         deepEqual(tree.items, {
@@ -279,84 +278,59 @@
             c: {}
         }, "should leave contents intact when attempting to delete a partially absent path");
 
-        result = tree.unsetPath('a>f>h>i>j>k>l>n>o'.toPath());
-        strictEqual(result, tree, "Tree.unsetPath is chainable");
-        deepEqual(
-            tree.items,
-            {
-                a: {d: {}, e: {}, f: {
-                    g: {}, h: {
-                        i: {j: {k: {m: {}}}}
-                    }
-                }},
-                b: {},
-                c: {}
-            },
-            "First path removed"
-        );
+        strictEqual(tree.unsetPath('a>f>h>i>j>k>l>n>o'.toPath()), tree, "should be chainable");
+
+        deepEqual(tree.items, {
+            a: {d: {}, e: {}, f: {
+                g: {}, h: {
+                    i: {j: {k: {m: {}}}}
+                }
+            }},
+            b: {},
+            c: {}
+        }, "should remove specified path");
 
         tree.unsetPath('a>f>h>i>j>k>m'.toPath(), false, function (path, value) {
-            equal(path.toString(), 'a>f>h', "Affected path");
-            equal(typeof value, 'undefined', "Affected value");
+            equal(path.toString(), 'a>f>h', "should pass affected path to handler");
+            equal(typeof value, 'undefined', "should pass affected value to handler");
         });
-        deepEqual(
-            tree.items,
-            {
+
+        deepEqual(tree.items, {
                 a: {d: {}, e: {}, f: {
                     g: {}
                 }},
                 b: {},
                 c: {}
-            },
-            "Second path removed"
-        );
+            }, "should remove specified path");
 
         tree.unsetPath('a>f>g'.toPath());
-        deepEqual(
-            tree.items,
-            {
+
+        deepEqual(tree.items, {
                 a: {d: {}, e: {}},
                 b: {},
                 c: {}
-            },
-            "Third path removed"
-        );
+            }, "should remove specified path");
 
         tree.unsetPath('b'.toPath());
-        deepEqual(
-            tree.items,
-            {
+        deepEqual(tree.items, {
                 a: {d: {}, e: {}},
                 c: {}
-            },
-            "Path ending in non-singular node removed"
-        );
+            }, "should remove path ending in non-singular node");
 
         tree.unsetPath('a>e>foo'.toPath());
-        deepEqual(
-            tree.items,
-            {
+        deepEqual(tree.items, {
                 a: {d: {}, e: {}},
                 c: {}
-            },
-            "Overreaching path not removed"
-        );
+            }, "should not remove overreaching node");
 
         tree
             .unsetPath('c'.toPath())
             .unsetPath('a>d'.toPath());
-        deepEqual(
-            tree.items,
-            {a: {e: {}}},
-            "Tree almost empty"
-        );
+
+        deepEqual(tree.items, {a: {e: {}}}, "should remove specified path");
 
         tree.unsetPath('a>e'.toPath());
-        deepEqual(
-            tree.items,
-            {},
-            "Tree completely emptied"
-        );
+        deepEqual(tree.items, {}, "should empty node after removing last remaining path");
     });
 
     test("Path deletion w/ empty path", function () {
